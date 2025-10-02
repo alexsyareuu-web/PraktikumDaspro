0ed113496a98639bca04760080507835c10acc51
alexsyareuu-web <alexsyareuu@gmail.com>
2025-10-02 11:15:03 +0700

    Percobaan 3

--- jobsheet5/nestedUjianSkripsi05.java
+++ jobsheet5/nestedUjianSkripsi05.java
@@ -16,11 +16,11 @@ public class nestedUjianSkripsi05 {
         
         if (bebasKompen.equalsIgnoreCase("Ya")) {
             if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
-                pesan = "Semua syarat terpenuhi. Mahasiswa bolrh mendaftar ujian skripsi";
+                pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
             } else if (bimbinganP1 < 8 && bimbinganP2 < 4 ) {
                 pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
             } else if (bimbinganP1 < 8) {
-                pesan = "Gagal! log bimbingan P1 belum mencapi 8 kali";
+                pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
             } else {
                 pesan = "Gagal Log bimbingan P2 belum mencapai 4 kali";
             }

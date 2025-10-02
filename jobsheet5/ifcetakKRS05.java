3ec617fcb80399b6b3f22c439baba2a94c61d799
alexsyareuu-web <alexsyareuu@gmail.com>
2025-10-02 10:15:16 +0700

    Modifikasi Percoabaan 2

--- jobsheet5/ifcetakKRS05.java
+++ jobsheet5/ifcetakKRS05.java
@@ -12,7 +12,7 @@ public class ifcetakKRS05 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
         } else {
-            System.out.println("Registrasi di tolak. Silahkan bayar UKT terlebih dshulu");
+            System.out.println("Registrasi di tolak. Silahkan bayar UKT terlebih dahulu");
         }
     }
 }
---
+++ jobsheet5/nestedUjianSkripsi05.java
@@ -0,0 +1,14 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class nestedUjianSkripsi05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pesan;
+        System.out.println("Apakah mahasiswa bebas kompen? (true/false): ");
+        String bebasKompen = sc.nextLine().trim();
+        
+    }
+    
+}

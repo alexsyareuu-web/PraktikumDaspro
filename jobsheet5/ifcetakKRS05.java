0e228ad932ccb4b4fb9ed4fd53436d3e813d12c4
alexsyareuu-web <alexsyareuu@gmail.com>
2025-10-02 08:34:31 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifcetakKRS05.java
+++ jobsheet5/ifcetakKRS05.java
@@ -11,6 +11,8 @@ public class ifcetakKRS05 {
         if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi di tolak. Silahkan bayar UKT terlebih dshulu");
         }
     }
 }

53ae1661e55fb8c664619d2b8864d4c89573a5c5
alexsyareuu-web <alexsyareuu@gmail.com>
2025-11-06 10:27:23 +0700

    ArrayNilai05.java

--- jobsheet9/ArrayNilai05.java
+++ jobsheet9/ArrayNilai05.java
@@ -8,16 +8,15 @@ public class ArrayNilai05 {
     
     int[] nilaiAkhir = new int[10];
     for (int i = 0; i < nilaiAkhir.length; i++) {
-        System.out.print( "masukkan nilai akhir ke-" + i + ": ");
-        nilaiAkhir [i] = sc.nextInt();
-       
+        System.out.print( "masukkan nilai akhir ke-" + (i+1) + ": ");
+        nilaiAkhir [i] = sc.nextInt(); 
     }
+    System.out.println("Daftar mahasiswa yang lulus (nilai > 70) :");
     for (int i = 0; i < nilaiAkhir.length; i++){
         if (nilaiAkhir [i] < 70){
             System.out.println("nilai akhir ke-" + i + " lulus");
-        } else {
-            System.out.println("nilai akhir ke-" + i + " tidak lulus");
-        }
+        } 
+        
     }
 }
 }
